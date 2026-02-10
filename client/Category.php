bc02532eb0c93e901f77a420a513e36b68b6f2ed
Atul Kumar Yadav <atulanirudhyadav@gmail.com>
2026-02-10 13:04:17 +0530

    category are dynamically implement with database

--- client/Category.php
+++ client/Category.php
@@ -1,6 +1,14 @@
 <select name="category" id="category" class="form-control dropdown">
     <option value="">Select Category</option>
-    <option value="IT">IT</option>
-    <option value="Python">Python</option>
-    <option value="Laravel">Laravel</option>
+    <?php
+    include("./common/db.php");
+    $catStmt = $con->query("Select * from categories");
+    $categories = $catStmt->fetch_all(MYSQLI_ASSOC);
+    foreach ($categories as $category) {
+    ?>
+        <option value="<?php echo $category['category'] ?>"><?php echo ucfirst($category['category']) ?></option>
+    <?php
+    }
+
+    ?>
 </select>
